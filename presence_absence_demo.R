b847ee0ffe462b0ae3ea809f8b22ef2cc5cc16cf
Anders Gravbrøt Finstad <anders.finstad@ntnu.no>
2017-11-14 22:07:57 +0100

    updated event_demo

--- presence_absence_demo.R
+++ presence_absence_demo.R
@@ -1,21 +1,13 @@
 ############################################################################################
-# download dataset https://www.gbif.org/dataset/c684b43a-0147-459b-ae97-0e3d9757fb92
+# download dataset https://www.gbif.org/dataset/40bfac0e-a1b5-4c9e-862e-64327195f210
 # Spread data to presence/absence by event according to occurrenceStatus
 ############################################################################################
 
-# First tried download as data after GBIF interpretation
-# Citation: GBIF Occurrence Download doi:10.15468/dl.dm359q accessed via GBIF.org on 14 Nov 2017
-temp <- tempfile()
-download.file(url=paste("http://api.gbif.org/v1/occurrence/download/request/0000810-171113114016250"),
-              destfile=temp,quiet=FALSE)
-data <- read.table(unz(temp, "0000810-171113114016250.csv"),sep="\t",header=T)
-str(data) # fails: does not contain the fields we need to interpret in terms of sampling-event
-
-
-# Download as Darwin Core archive ------
+# Download as Darwin Core archive.
+# Download as data after GBIF interpretation will fail due to missing fields
 # Citation: GBIF Occurrence Download doi:10.15468/dl.7s1c2n accessed via GBIF.org on 14 Nov 2017
 temp <- tempfile()
-download.file(url=paste("http://api.gbif.org/v1/occurrence/download/request/0000818-171113114016250"),
+download.file(url=paste("http://api.gbif.org/v1/occurrence/download/request/0000906-171113114016250"),
               destfile=temp,quiet=FALSE)
 evdata <- read.table(unz(temp, "occurrence.txt"),sep="\t",header=T)
 str(evdata) # oki, here we have what we need
@@ -47,6 +39,12 @@ spread_data <- evdata2 %>%
 evdata_tmp <- distinct(evdata[c("eventID","samplingProtocol","eventDate")])
 spread_data <- left_join(spread_data,evdata_tmp, by="eventID")
 
-
+#-------------------------------------------------------------
+# Get additiona habitat data for occupancy modelling ---------
+#
+# Appears to be encoded in the eventID. Go to orginal dataset
+# endpoint to get measurment and fact table...
+#
+#-------------------------------------------------------------
 
 
