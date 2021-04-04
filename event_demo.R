2592a07ff2d3cbb7600e5b8d162dd91fc7cca356
andersfi <anders.finstad@ntnu.no>
2021-04-04 11:03:20 +0200

    added new file

--- event_demo.R
+++
@@ -1,5 +0,0 @@
-# download and wrangle GBIF mediated event datatset
-
-# Using the microcrustacean time series from lake Jonsvatnet, central Norway as exampel.
-
-download_url <- "https://doi.org/10.15468/dl.m55nyd"
